eff98a2c5d2653a667e47f6ed1354dfa8e63032e
yizhongw <eastonwyz@gmail.com>
2022-05-26 15:00:54 -0700

    update GPT3 running scripts

--- scripts/run_gpt3.sh
+++ scripts/run_gpt3.sh
@@ -1,17 +1,18 @@
-export openai_key="Your OpenAI api key"
+export openai_key="Your OpenAI API key"
 
 data_dir=data/splits/default
 task_dir=data/tasks
-output_dir=output/gpt3
+output_dir=output/
 max_num_instances_per_eval_task=20
 
 echo "instruction + 2 positive examples"
-for engine in "davinci" "text-ada-001"
+for engine in "text-davinci-001" "davinci" 
 do
 echo $engine
 python src/run_gpt3.py \
     --data_dir $data_dir \
     --task_dir $task_dir \
+    --overwrite_cache \
     --max_num_instances_per_task 1 \
     --max_num_instances_per_eval_task ${max_num_instances_per_eval_task} \
     --add_task_definition True \
@@ -21,8 +22,8 @@ python src/run_gpt3.py \
     --max_source_length 1024 \
     --max_target_length 128 \
     --engine ${engine} \
-    --output_dir ${output_dir}/default/${engine}
-python src/compute_metrics.py ${output_dir}/default/${engine}/gpt3_predictions.json
+    --output_dir ${output_dir}/default/gpt3/${engine}
+python src/compute_metrics.py --predictions ${output_dir}/default/gpt3/${engine}/predicted_examples.json --track default
 done
 
 echo "xlingual instruction + 2 positive examples"
@@ -32,6 +33,7 @@ echo $engine
 python src/run_gpt3.py \
     --data_dir data/splits/xlingual/ \
     --task_dir $task_dir \
+    --overwrite_cache \
     --max_num_instances_per_task 1 \
     --max_num_instances_per_eval_task ${max_num_instances_per_eval_task} \
     --add_task_definition True \
@@ -41,6 +43,6 @@ python src/run_gpt3.py \
     --max_source_length 1024 \
     --max_target_length 128 \
     --engine ${engine} \
-    --output_dir ${output_dir}/xlingual/${engine}
-python src/compute_metrics.py ${output_dir}/xlingual/${engine}/gpt3_predictions.json
+    --output_dir ${output_dir}/xlingual/gp3/${engine}
+python src/compute_metrics.py --predictions ${output_dir}/xlingual/gpt3/${engine}/predicted_examples.json --track xlingual
 done
--- src/run_gpt3.py
+++ src/run_gpt3.py
@@ -64,13 +64,13 @@ if __name__ == "__main__":
         json.dump(args.__dict__, fout)
 
     existing_requests = {}
-    if os.path.exists(os.path.join(args.output_dir, "gpt3_predictions.json")):
-        with open(os.path.join(args.output_dir, "gpt3_predictions.json")) as fin:
+    if os.path.exists(os.path.join(args.output_dir, "predicted_examples.json")):
+        with open(os.path.join(args.output_dir, "predicted_examples.json")) as fin:
             for line in fin:
                 request_info = json.loads(line)
                 existing_requests[request_info["gpt3_input"]] = request_info["gpt3_response"]
 
-    with open(os.path.join(args.output_dir, "gpt3_predictions.json"), "w") as fout:
+    with open(os.path.join(args.output_dir, "predicted_examples.json"), "w") as fout:
         for example in tqdm.tqdm(raw_datasets["test"]):
             encoded_example = data_collator([example])
             example["gpt3_input"] = encoded_example["inputs"][0].strip()
@@ -88,6 +88,8 @@ if __name__ == "__main__":
                     presence_penalty=0
                 )
             example["gpt3_response"] = response
+            # Note: we cut the generated text at the first period, since the GPT3 language model sometimes generates more than one sentences.
+            # Our results show that this won't affect the instruct-GPT3 model very much, but will significantly improve the original GPT3 LM.
             example["prediction"] = example["gpt3_response"]["choices"][0]["text"].strip().split(".")[0]
             fout.write(json.dumps(example) + "\n")
 
